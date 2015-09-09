31ad274a889bf2bf567a78b28f7c74e05152082f
MAC <lilili@LILI.local>
2015-09-09 16:56:33 +0800

    Initial Commit

---
+++ alltuu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alltuu
+//
+//  Created by MAC on 15/9/9.
+//  Copyright (c) 2015年 Alltuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
